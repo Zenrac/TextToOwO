b2e8c122dc93db28f7cdea5d70c92ee7e3a91d7b
Zenrac <zenrac@outlook.fr>
2018-10-07 23:07:51 +0200

    go PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'OwO-Translator',
+    packages = [],
+    version = 'v1.0',
+    description = 'OwO',
+    author = 'Zenrac',
+    author_email = 'zenrac@outlook.fr',
+    url = 'https://github.com/Zenrac/OwO-Translator/',
+    download_url = 'https://github.com/Zenrac/OwO-Translator/archive/v1.0.tar.gz',
+    keywords = ['OwO', 'Convewtew'],
+    classifiers = [],
+)
