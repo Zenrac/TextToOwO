ac6a6761c08aa6ee142d0f76f2f7354363db3809
Zenrac <zenrac@outlook.fr>
2018-10-08 19:55:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = 'TextToOwO',
     packages = ['TextToOwO'],
-    version = 'v1.1.2',
+    version = 'v1.1.3',
     description = 'OwO',
     author = 'Zenrac',
     author_email = 'zenrac@outlook.fr',
     url = 'https://github.com/Zenrac/OwO-Translator/',
-    download_url = 'https://github.com/Zenrac/TextToOwO/archive/v1.1.2.tar.gz',
+    download_url = 'https://github.com/Zenrac/TextToOwO/archive/v1.1.3.tar.gz',
     keywords = ['OwO', 'Convewtew'],
     classifiers = [],
 )
