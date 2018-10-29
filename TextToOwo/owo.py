84cb5365f4ba9640d034ce810539f38de7320460
Zenrac <zenrac@outlook.fr>
2018-10-29 15:07:34 +0100

    ready to v2
    thx @Devoxin :^)

--- TextToOwo/owo.py
+++ TextToOwo/owo.py
@@ -1,7 +1,7 @@
 import re
 import random
 
-vowels = ['a','e','i','o','u','A','E','I','O','U']
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
 
 def last_replace(s, old, new):
     li = s.rsplit(old, 1)
@@ -9,32 +9,27 @@ def last_replace(s, old, new):
 
 def text_to_owo(text):
     """ Converts your text to OwO """
-    smileys = [';;w;;', '^w^', '>w<', 'UwU']
-    if '`' not in text:
-        smileys.append("(・`ω´・)")   
-    
+    smileys = [';;w;;', '^w^', '>w<', 'UwU', '(・`ω\´・)', '(´・ω・\`)']
+
     text = text.replace('L', 'W').replace('l', 'w')
     text = text.replace('R', 'W').replace('r', 'w')
-    
+
     while '!!' in text:
         text = text.replace('!!', '!')
     while '??' in text:
         text = text.replace('??', '?')
-        
-    chosen = random.choice(smileys) # Don't want the same smiley twice in the same text.
-    smileys.remove(chosen)
-    
-    text = last_replace(text, '!', ' {}'.format(chosen))
+
+    text = last_replace(text, '!', ' {}'.format(random.choice(smileys)))
     text = last_replace(text, '?', '? owo')
     text = last_replace(text, '...', ' UwU')
     text = last_replace(text, '.', '. {}'.format(random.choice(smileys)))
-    
-    text = re.sub('\s\s+' , ' ', text) # remove useless space
-    
+
+    text = re.sub('\s\s+', ' ', text)  # remove useless space
+
     for v in vowels:
         if 'n{}'.format(v) in text:
             text = text.replace('n{}'.format(v), 'ny{}'.format(v))
         if 'N{}'.format(v) in text:
             text = text.replace('N{}'.format(v), 'N{}{}'.format('Y' if v.isupper() else 'y', v))
-            
+
     return text
